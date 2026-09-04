94dfa25f3a83beffca7bad3347c57ca5d6baa2ac
Aayush Raj <aayushsbrajput@gmail.com>
2026-09-04 22:45:38 +0530

    Configure H2 database for CI tests

--- src/test/java/com/ecommerce/productcatalogservice/ProductCatalogServiceApplicationTests.java
+++ src/test/java/com/ecommerce/productcatalogservice/ProductCatalogServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ecommerce.productcatalogservice;
+import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductCatalogServiceApplicationTests {
 
     @Test
